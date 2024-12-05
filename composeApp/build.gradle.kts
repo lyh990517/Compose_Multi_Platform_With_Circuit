a25c5faefa759c5f21ff3b5d32d3ecf48b03a887
lyh990517 <lyh990517@naver.com>
2024-12-05 10:01:50 +0900

    gradle setting

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,24 +1,29 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+import java.util.regex.Pattern
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.buildKonfig)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -29,9 +34,9 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     jvm("desktop")
-    
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         moduleName = "composeApp"
@@ -51,27 +56,80 @@ kotlin {
         }
         binaries.executable()
     }
-    
+
     sourceSets {
-        val desktopMain by getting
-        
-        androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
-        }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.navigation.compose)
+            implementation(libs.compose.adaptive)
+            implementation(libs.kotlinx.coroutines)
+            implementation(libs.coil.network.ktor)
+            implementation(libs.bundles.ktor)
+            implementation(libs.coil.compose)
+            api(libs.koin.core)
+            implementation(libs.koin.compose.viewmodel)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.okio)
+            implementation(libs.multiplatformSettings.no.arg)
         }
+        commonTest.dependencies {
+            implementation(kotlin("test-annotations-common"))
+            implementation(libs.kotlin.test)
+            implementation(libs.multiplatformSettings.test)
+            @OptIn(ExperimentalComposeLibrary::class) implementation(compose.uiTest)
+        }
+
+        val desktopMain by getting
+
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.android)
+            implementation(libs.koin.compose)
+            implementation(libs.ktor.client.okhttp)
+            implementation(libs.androidx.core.splashscreen)
+            implementation(libs.androidx.room.runtime)
+            implementation(libs.sqlite.bundled)
+        }
+
+        androidUnitTest.dependencies {
+            implementation(libs.mockk)
+        }
+
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.androidx.room.runtime)
+            implementation(libs.sqlite.bundled)
+        }
+
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
+            implementation(libs.ktor.client.okhttp)
             implementation(libs.kotlinx.coroutines.swing)
+            implementation(libs.androidx.room.runtime)
+            implementation(libs.sqlite.bundled)
+        }
+
+        wasmJsMain.dependencies {
+
+        }
+
+        wasmJsTest.dependencies {
+
+        }
+
+        dependencies {
+            ksp(libs.androidx.room.compiler)
+        }
+
+        room {
+            schemaDirectory("$projectDir/schemas")
         }
     }
 }
@@ -93,13 +151,34 @@ android {
         }
     }
     buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+
+    flavorDimensions.add("variant")
+    productFlavors {
+        create("Dev") {
+            dimension = "variant"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = " Dev"
+            resValue("string", "app_name_variant", "ComposeMultiPlatform")
+        }
+
+        create("Prod") {
+            isDefault = true
+            dimension = "variant"
+            resValue("string", "app_name_variant", "ComposeMultiPlatform")
+        }
     }
 }
 
@@ -117,4 +196,48 @@ compose.desktop {
             packageVersion = "1.0.0"
         }
     }
+}
+
+// Ref: https://sujanpoudel.me/blogs/managing-configurations-for-different-environments-in-kmp/
+project.extra.set("buildkonfig.flavor", currentBuildVariant())
+buildkonfig {
+    packageName = "ComposeMultiPlatform"
+    objectName = "CMPConfig"
+    exposeObjectWithName = "CMPConfig"
+
+    defaultConfigs {
+
+    }
+
+    defaultConfigs("Dev") {
+
+    }
+
+    defaultConfigs("Prod") {
+
+    }
+}
+
+fun Project.getAndroidBuildVariantOrNull(): String? {
+    val variants = setOf("Dev", "Prod")
+    val taskRequestsStr = gradle.startParameter.taskRequests.toString()
+    val pattern: Pattern = if (taskRequestsStr.contains("assemble")) {
+        Pattern.compile("assemble(\\w+)(Release|Debug)")
+    } else {
+        Pattern.compile("bundle(\\w+)(Release|Debug)")
+    }
+
+    val matcher = pattern.matcher(taskRequestsStr)
+    val variant = if (matcher.find()) matcher.group(1) else null
+    return if (variant in variants) {
+        variant
+    } else {
+        null
+    }
+}
+
+private fun Project.currentBuildVariant(): String {
+    val variants = setOf("Dev", "Prod")
+    return getAndroidBuildVariantOrNull() ?: System.getenv()["VARIANT"].toString()
+        .takeIf { it in variants } ?: "Dev"
 }
